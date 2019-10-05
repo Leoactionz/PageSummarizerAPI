080081f3caf4748e7df8b6166194e6511be31a15
Leo <leoucu@gmail.com>
2019-10-05 14:17:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
+import pickle
 from flask import Flask, render_template, url_for, request, jsonify
 import pandas as pd
-import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import joblib
 
 # load model
-Countvec = pickle.load(open("models/Countvec.pkl", "rb"))
-spammodel = open('models/spammodel.pkl', 'rb')
-clf = joblib.load(spammodel)
+COUNTVEC = pickle.load(open("models/Countvec.pkl", "rb"))
+SPAMMODEL = open('models/spammodel.pkl', 'rb')
+CLF = joblib.load(SPAMMODEL)
 
 # app
-app = Flask(__name__)
+APP = Flask(__name__)
 
 # routes
 
 
-@app.route('/', methods=['GET'])
+@APP.route('/', methods=['GET'])
 def home():
     # Give message to user
     return {"message": "This is SMS spam detection model. Use the format {'message': 'SMS message'} and POST to get prediction."}
 
 
-@app.route('/', methods=['POST'])
+@APP.route('/', methods=['POST'])
 def predict():
     # get data
     data = request.get_json(force=True)
     data = data['message']
     data = [data]
     # convert data into array
-    arrt = Countvec.transform(data).toarray()
+    arrt = COUNTVEC.transform(data).toarray()
 
     # predictions
-    my_prediction = clf.predict(arrt)
+    my_prediction = CLF.predict(arrt)
 
     # check predicted value
     if my_prediction[0] == 0:
@@ -45,4 +45,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    APP.run(port=5000, debug=True)
